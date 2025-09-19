64879bf0c54522626d56918da8eac5f82c39aba4
Hendrik <161131951+brownhamer@users.noreply.github.com>
2025-09-19 20:12:15 +0200

    Restyling my alternatives listing

--- alternatives.php
+++ alternatives.php
@@ -8,22 +8,17 @@ startHtmlPage('Alt0.nl &rarr; Alternatives');
 #============================================================================ ?>
 
 <h1><a href="<?php echo $alt0_url; ?>"><span class="link-button">Alt0.nl</span></a></h1>
-<h3><?php
-	echo oneLiner();
-	if ($links_id === '') {
-		echo "<br /><a href=\"".$alt0_url."?links=oneliners\">...</a>\n";
-	}
-?></h3>
+<h3><?php	echo oneLiner(); ?></h3>
 <div id="spacer">&nbsp;</div>
 
 <table id="alternatives"><tr><td>
 <div id="choices"><h5>My choices:</h5>
 <ul>
-  <li>Browser <span class="del">Chrome</span> &rarr; <b><a href="https://www.firefox.com/">Firefox</a></b></li>
-  <li>Cloud storage <span class="del">OneDrive</span> &rarr; <b><a href="https://nextcloud.com/">NextCloud</a></b> via <b><a href="https://ocloud.de/">oCloud</a></b></li>
-  <li>Messenger <span class="del">WhatsApp</span> &rarr; <b><a href="https://signal.org/">Signal</a></b></li>
-  <li>Navigation <span class="del">Maps</span> &rarr; <b><a href="https://www.comaps.app/">CoMaps</a></b> and <b><a href="https://wego.here.com/">HERE WeGo</a></b></li>
-  <li>Search <span class="del">Google</span> &rarr; <b><a href="https://start.duckduckgo.com/">DuckDuckGo</a></b></li>
+  <li>Browser: <span class="del">Chrome</span> &rarr; <a href="https://www.firefox.com/">Firefox</a></li>
+  <li>Cloud storage: <span class="del">OneDrive</span> &rarr; <a href="https://nextcloud.com/">NextCloud</a> via <a href="https://ocloud.de/">oCloud</a></li>
+  <li>Messenger: <span class="del">WhatsApp</span> &rarr; <a href="https://signal.org/">Signal</a></li>
+  <li>Navigation: <span class="del">Maps</span> &rarr; <a href="https://www.comaps.app/">CoMaps</a> and <a href="https://wego.here.com/">HERE WeGo</a></li>
+  <li>Search: <span class="del">Google</span> &rarr; <a href="https://start.duckduckgo.com/">DuckDuckGo</a></li>
 </ul>
 </div>
 </td><td>
