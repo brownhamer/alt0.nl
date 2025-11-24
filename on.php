55ad8bb42e8cd8ed18cf18689bd6c8f330bff738
Hendrik <hendrik@alt0.nl>
2025-11-24 21:10:33 +0100

    More on Meta

--- on.php
+++ on.php
@@ -36,11 +36,13 @@ startHtmlPage('Alt0.nl &rarr; On AI');
 
 <h5>On Meta:</h5>
 <ul>
+  <li><a href="https://www.dedigitaletuin.nl/overig/facebook-schandalen/">Facebook schandalen</a> (165+)</li>
   <li><a href="https://tweakers.net/nieuws/203926/meta-krijgt-265-miljoen-euro-avg-boete-om-gescrapete-data-van-533-miljoen-users.html">Meta krijgt 265 miljoen euro AVG-boete om gescrapete data van 533 miljoen users</a></li>
   <li><a href="https://tweakers.net/nieuws/211816/noorse-privacywaakhond-geeft-meta-88000-euro-boete-per-dag-voor-overtreden-avg.html">Noorse privacywaakhond geeft Meta 88.000 euro boete per dag voor overtreden AVG</a></li>
   <li><a href="https://tweakers.net/nieuws/235760/meta-had-vanaf-september-inzage-in-surfgedrag-van-android-gebruikers.html">Meta had vanaf september inzage in surfgedrag van Android-gebruikers</a></li>
   <li><a href="https://tweakers.net/nieuws/237058/meta-moet-duitser-5000-euro-schadevergoeding-betalen-vanwege-illegale-tracking.html">Meta moet Duitser 5000 euro schadevergoeding betalen vanwege illegale tracking</a></li>
   <li><a href="https://decorrespondent.nl/16421/mag-meta-bepalen-wat-jij-ziet-op-instagram-en-facebook-nee-vindt-de-rechter/3a5ebc2a-ba4a-0a90-22f6-cb9a2c021dc5">Mag Meta bepalen wat jij ziet op Instagram en Facebook? Nee, vindt de rechter</a></li>
+  <li><a href="https://tweakers.net/nieuws/241810/meta-verstopte-onderzoek-dat-wees-op-negatieve-effecten-facebook-en-instagram.html">Meta verstopte onderzoek dat wees op negatieve effecten Facebook en Instagram</a></li>
 </ul>
 
 <h5>On Privacy:</h5>
